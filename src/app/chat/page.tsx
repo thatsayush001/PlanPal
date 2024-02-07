8ad6056bedcf694c6aaa85b07e970f1aa5d77c67
thatsayush001 <iit2022010@iiitl.ac.in>
2024-02-07 14:50:50 +0530

    fixed chat bug of messages not being updated in database

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -13,9 +13,9 @@ const page = () => {
   const router = useRouter();
   const [socket, setSocket] = useState<any>(undefined);
   const [inbox, setInbox] = useState<
-    Array<{ sender: string; message: string; date: string;avatar_url:string }>
+    Array<{ sender: string; message: string; date: string; avatar_url: string }>
   >([]);
-  const [userAvatar,setUserAvatar] = useState("");
+  const [userAvatar, setUserAvatar] = useState("");
   const [message, setMessage] = useState<any>("");
   const [roomName, setRoomName] = useState<any>("");
   const [sender, setSender] = useState("");
@@ -48,8 +48,8 @@ const page = () => {
         setCurrentUser(data.currentUser);
         setSender(data.currentUser?.username);
         setUserAvatar(data.currentUser?.avatar);
-        if(data.currentUser?.rooms.length>0){
-          setRoomName(data.currentUser?.rooms[0])
+        if (data.currentUser?.rooms.length > 0) {
+          setRoomName(data.currentUser?.rooms[0]);
         }
       }
     } catch (error) {
@@ -58,46 +58,46 @@ const page = () => {
   };
   const updateInbox = async () => {
     try {
-      const response = await axios.put('/api/addMessageToInbox', {
+      const response = await axios.put("/api/addMessageToInbox", {
         id: roomName,
         time: new Date(),
         sender: sender,
         message: message,
         avatar_url: userAvatar,
       });
-  
-      console.log('Response:', response.data);
+
+      console.log("Response:", response.data);
     } catch (error) {
-      console.log('Error:', error);
+      console.log("Error:", error);
     }
   };
   useEffect(() => {
     const socket = io("https://github-finder-server.onrender.com");
     // const socket = io("http://localhost:3001");
-    socket.on("message", (message, sender, date,avatar_url) => {
+    socket.on("message", (message, sender, date, avatar_url) => {
       const newMessage = {
         sender: sender,
         message: message,
         date: date,
-        avatar_url : avatar_url
+        avatar_url: avatar_url,
       };
       setInbox((prevInbox) => [...prevInbox, newMessage]);
     });
     setSocket(socket);
   }, []);
-  useEffect(()=>{
-    if(session?.user?.email){
-     fetchCurrentUserData();
+  useEffect(() => {
+    if (session?.user?.email) {
+      fetchCurrentUserData();
     }
-  },[session?.user?.email])
+  }, [session?.user?.email]);
   const handleSendMessage = () => {
-    socket.emit("message", message, roomName, sender, new Date(),userAvatar);
+    socket.emit("message", message, roomName, sender, new Date(), userAvatar);
     updateInbox();
   };
   const handleJoinRoom = (room: string) => {
     socket?.emit("joinRoom", room);
   };
-  const getRoom = async()=>{
+  const getRoom = async () => {
     try {
       const res = await fetch(`/api/getRoomById?id=${roomName}`);
       if (!res.ok) {
@@ -108,26 +108,32 @@ const page = () => {
     } catch (error) {
       console.log("Error loading Rooms: ", error);
     }
-  }
-  const setRoom = async ()=>{
-    const room = await getRoom()
-      setCurrentRoom(room.currentRoom);
-      setInbox(room.currentRoom.messages)
-  }
-  useEffect(()=>{
+  };
+  const setRoom = async () => {
+    const room = await getRoom();
+    setCurrentRoom(room.currentRoom);
+    setInbox(room.currentRoom.messages);
+  };
+  useEffect(() => {
     handleJoinRoom(roomName);
-    if(roomName!=""){
+    if (roomName != "") {
       setRoom();
     }
-  },[roomName])
+  }, [roomName]);
   return (
     <div className="flex h-screen">
       {/* Left Sidebar (Inbox) */}
       <div className="w-1/4 bg-red-900 p-4">
         <h1 className="text-xl font-bold">Inbox</h1>
         <ul>
-          {(currentUser as any)?.rooms.map((item:any,i:any) => (
-            <button key={i} className="py-2 border rounded text-sm" onClick={()=>{setRoomName(item)}}>
+          {(currentUser as any)?.rooms.map((item: any, i: any) => (
+            <button
+              key={i}
+              className="py-2 border rounded text-sm"
+              onClick={() => {
+                setRoomName(item);
+              }}
+            >
               {item}
             </button>
           ))}
@@ -135,35 +141,47 @@ const page = () => {
       </div>
 
       {/* Right Chat Section */}
-      {roomName!=""? (<div className="w-3/4 p-4 bg-blue-900 overflow-scroll">
-        Members : 
-        <div className="flex flex-row gap-3">
-         {(currentRoom as any)?.members?.map((m:any,i:any)=>{
-          return <button key={i} onClick={() => router.push(`/profile/${m}`)}>{m}</button>
-         })}
+      {roomName != "" ? (
+        <div className="w-3/4 p-4 bg-blue-900 overflow-scroll">
+          Members :
+          <div className="flex flex-row gap-3">
+            {(currentRoom as any)?.members?.map((m: any, i: any) => {
+              return (
+                <button key={i} onClick={() => router.push(`/profile/${m}`)}>
+                  {m}
+                </button>
+              );
+            })}
+          </div>
+          <h1 className="text-xl font-bold">Chat</h1>
+          <div>
+            {inbox.map((i: any, id: any) => (
+              <div className="py-2" key={id}>
+                {i.message} by : {i.sender}
+                {/* at : {formatISODate(i.date)} */}
+                <img
+                  src={i.avatar_url}
+                  alt="lolo"
+                  className="rounded-full w-16 h-16 object-cover"
+                />
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-row">
+            <input
+              className="text-black"
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+            />
+            <button onClick={handleSendMessage}>Send</button>
+          </div>
         </div>
-        <h1 className="text-xl font-bold">Chat</h1>
-        <div>
-          {inbox.map((i: any,id:any) => (
-            <div className="py-2" key={id}>
-              {i.message} by : {i.sender}
-              {/* at : {formatISODate(i.date)} */}
-              <img src={i.avatar_url} alt="lolo" className="rounded-full w-16 h-16 object-cover"/>
-            </div>
-          ))}
+      ) : (
+        <div className="text-xl font-bold text-gray-700 mt-4">
+          You have no rooms
         </div>
-        <div className="flex flex-row">
-          <input
-            className="text-black"
-            onChange={(e) => {
-              setMessage(e.target.value);
-            }}
-          />
-          <button onClick={handleSendMessage}>Send</button>
-        </div>
-      </div>):<div className="text-xl font-bold text-gray-700 mt-4">
-    You have no rooms
-  </div>}
+      )}
     </div>
   );
 };
