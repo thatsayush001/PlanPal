12e301d968a65c9b933d4af9d2992dc1734c604c
thatsayush001 <iit2022010@iiitl.ac.in>
2024-03-14 23:51:23 +0530

    added input field and list of users

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,41 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
 import { FiAlignRight } from "react-icons/fi";
 import { CiSearch } from "react-icons/ci";
 
 const Navbar = () => {
+  const [allUsers, setAllUsers] = useState(null);
+  const [inputText, setInputText] = useState("");
+  const getUsers = async () => {
+    try {
+      const res = await fetch("/api/fetchAllUsers");
+      if (!res.ok) {
+        throw new Error("Failed to fetch users");
+      }
+
+      return res.json();
+    } catch (error) {
+      console.log("Error loading users: ", error);
+    }
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getUsers();
+        if (data) {
+          console.log(data.allUsers);
+          setAllUsers(data.allUsers);
+        }
+      } catch (error) {
+        console.error("Error fetching hackathons: ", error);
+      }
+    };
+
+    fetchData();
+  }, [inputText]);
+
   const { data: session }: any = useSession();
   return (
     <div className=" w-screen bg-[rgb(10,1,31)] rounded-lg border-b border-gray-500 ">
@@ -31,18 +61,15 @@ const Navbar = () => {
                 </li>
               </Link>
             </div>
-            <Link href="/" passHref>
-              <li className="hover:text-gray-300 text-sm pt-2 font-serif">
-                <div className="border bg-[rgb(10,1,31)] border-gray-600 rounded w-48 h-7 align-middle flex ">
-                  <CiSearch className=" w-7   h-5 align-middle mt-1" />
-                  <div className="bg-[rgb(10,1,31)]  rounded w-[100%] h-[100%]  flex justify-end" >
-                    <p className="mt-1 mr-1 text-gray-600">⌘K</p>
-                  </div>
-                </div>
-
-                {/* Need to add input */}
-              </li>
-            </Link>
+            <li className="text-black pt-2 font-serif">
+              <input
+                className=" w-full h-5 align-middle mt-1"
+                value={inputText}
+                onChange={(e: any) => {
+                  setInputText(e.target.value);
+                }}
+              />
+            </li>
             <div className="flex gap-10  invisible md:visible absolute md:relative">
               {session ? (
                 <Link href="/chat" passHref>
